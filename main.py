6d023fb44386c4c54b3985eb37a01ae05aea0bb2
Naresh Bokka <63376063+Nareshboks@users.noreply.github.com>
2025-11-12 13:21:29 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_classic.chains import LLMChain
-from langchain_classic.prompts import PromptTemplate
+# from langchain_classic.chains import LLMChain
+from langchain_core.prompts import PromptTemplate
 
 import streamlit as st
 import os
@@ -14,7 +14,7 @@ tweet_template = "Give me {number} tweets on {topic}"
 tweet_prompt = PromptTemplate(template = tweet_template, input_variables = ['number', 'topic'])
 
 # Initialize Google's Gemini model
-gemini_model = ChatGoogleGenerativeAI(model = "gemini-1.5-flash-latest")
+gemini_model = ChatGoogleGenerativeAI(model = "gemini-2.0-flash")
 
 
 # Create LLM chain using the prompt template and model
@@ -32,6 +32,7 @@ topic = st.text_input("Topic")
 number = st.number_input("Number of tweets", min_value = 1, max_value = 10, value = 1, step = 1)
 
 if st.button("Generate"):
+    st.write("Generating Tweets ....")
     tweets = tweet_chain.invoke({"number" : number, "topic" : topic})
     st.write(tweets.content)
     
